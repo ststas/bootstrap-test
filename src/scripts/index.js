024828a5211ca5f9552f5e489bba6bd36d0669bf
Stas Zaitsev <ststas@yandex.ru>
2023-08-18 20:30:27 +0300

    update footer & modals

---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+
+$('.project-popup-link').magnificPopup({
+  type: 'image',
+  mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+  zoom: {
+    enabled: true, // By default it's false, so don't forget to enable it
+
+    duration: 300, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+
+    opener: function(openerElement) {
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+    }
+  }
+});
